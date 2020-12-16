e73726b02e48fb074256f9572e7377374b18aa28
Shiva Raju <shiva.raju@esure.com>
2020-12-16 11:33:23 +0000

    codeceptjs tests for bp qbmotor

---
+++ PolicyJourney_test.js
@@ -0,0 +1,25 @@
+Feature('PolicyJourney');
+
+Scenario('test something', ({ I }) => {
+    I.amOnPage('/ux1/review');
+    I.see('Protected NCD', '[data-qa="pncd"]');
+    I.see('We will protect your 13 years no claims discount', '[data-qa="pncd"]~p');
+    I.see('Motor Legal Cover', '[data-qa="lCover"]');
+    I.see('Good news Paul!');
+    I.see('Benefits included as standard');
+    I.see('£200.00', '[data-qa="totalExcess"]');
+    I.click('Total Excess');
+    I.click('[data-qa="continueBtnDialog"]');
+    I.saveScreenshot('ReviewPage.png');
+    I.click('div[data-qa="buyBtnFooter"] button');
+    // Buy page
+    I.see('All payments are safe and secure');
+    I.saveScreenshot('BuyPage.png');
+    I.click('Pay Now');
+    // drive page
+    I.see('Welcome on board');
+    I.see('PL0321789', '[data-qa="drivePolicyNumber"]');
+    I.see('15 December 2020', '[data-qa="drivePolicyStartDate"]');
+    I.see('Home Insurance');
+    I.saveScreenshot('DrivePage.png');
+});
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Codeception_POC
+
+CodeceptJS is a testing framework. But unlike other testing tools for JavaScript, it uses most popular JS libraries to drive your browser and adds high-level API on top of them.CodeceptJS is a Meta Test framework with various helpers
+
+## **Installation**
+
+•**Initialise the project :**
+
+     npm init –y
+
+•**Install pupeteer :**
+
+     npm install codeceptjs puppeteer --save-dev
+
+•**Initialise Codecept:**
+
+     npx codeceptjs init
+
+•**What helpers do you want to use?**
+
+    Choose the helper e.g. Puppeteer
+
+•**Create a Test :**
+
+    npx codeceptjs gt - Give a file name
+
+**Run the tests in steps wise**
+
+    npx codeceptjs run --steps
+
+**Generate mochawesome reports**
+
+    npx codeceptjs run --reporter mochawesome
